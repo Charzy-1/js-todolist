ecb34fbf50d5379f15782fa5e0263fa220b30cb2
Charles Adikankwu <chazylites@gmail.com>
2026-09-24 16:03:03 +0100

    Added a funtion to render the todo on the webpage using DOM

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // Create and empty array to store todos
-  const myTodo = [];
-  
-  function addButton() {
+const myTodo = [''];
+
+function renderTodoList() {
+  let todoListHtml = '';
+
+  // LOOP TRHOUGH THE ARRAY USING FOR LOOP
+  for (let i = 0; i < myTodo.length; i++) {
+    const todo = myTodo[i];
+    // CREATE SOME HTML CODE FOR EACH TODO
+    const html = `<p>${todo}</p>`;
+    todoListHtml = todoListHtml + html;
+  }
+
+  // put the html on the webpage
+  document.querySelector('.js-todos')
+    .innerHTML = todoListHtml;
+}
+
+function addButton() {
   // Get hold of the todos from the user
   const inputElement = document.querySelector('.inputBox');
   const todoText = inputElement.value;
 
   myTodo.push(todoText);
 
-  // Push the user input into the empty array
-  console.log(myTodo);
-
   // Clear the textbox after adding the todo
   inputElement.value = '';
+
+  renderTodoList()
 }

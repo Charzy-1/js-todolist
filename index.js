82c1e51f3a75342f454a43ac28e2cf2c1f6f12c8
Charles Adikankwu <chazylites@gmail.com>
2026-09-24 16:49:10 +0100

    added the delete funtionality and also added keydown event to render todolist when user press enter button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Create and empty array to store todos
-const myTodo = [''];
+const myTodo = [];
 
 function renderTodoList() {
   let todoListHtml = '';
@@ -7,10 +7,30 @@ function renderTodoList() {
   // LOOP TRHOUGH THE ARRAY USING FOR LOOP
   for (let i = 0; i < myTodo.length; i++) {
     const todo = myTodo[i];
+
     // CREATE SOME HTML CODE FOR EACH TODO
-    const html = `<p>${todo}</p>`;
+    const html = 
+    `<p>
+      ${todo}
+      <button onclick='
+        myTodo.splice(${i}, 1);
+        renderTodoList();
+      '>
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="3 6 5 6 21 6"></polyline>
+          <path d="M19 6l-1 14H6L5 6"></path>
+          <path d="M10 11v6"></path>
+          <path d="M14 11v6"></path>
+          <path d="M9 6V4h6v2"></path>
+        </svg>
+      </button>
+    </p>`;
     todoListHtml = todoListHtml + html;
   }
+
+  console.log(todoListHtml)
 
   // put the html on the webpage
   document.querySelector('.js-todos')
@@ -28,4 +48,11 @@ function addButton() {
   inputElement.value = '';
 
   renderTodoList()
+}
+
+// FUNTION TO ADD THE MY TODOLIST ON THE PAGE WHEN THE ENTER BUTTON IS PRESSED
+function renderTodoWithEnterKey(event) {
+  if (event.key === 'Enter') {
+    addButton()
+  }
 }
